8e4d86ff53b3f8402b6cdfb702d075fc8b2bd151
Shindongha <tlsehdgk33@gmail.com>
2022-10-05 10:50:08 +0900

    2

---
+++ LEDCUBE/app/src/main/java/com/example/ledcube/draw/DrawActivity.java
@@ -0,0 +1,69 @@
+package com.example.ledcube.draw;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.divyanshu.draw.widget.DrawView;
+import com.example.ledcube.R;
+import com.example.ledcube.tflite.Classifier;
+
+import java.io.IOException;
+import java.util.Locale;
+
+public class DrawActivity extends AppCompatActivity {
+
+    Classifier cls;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+
+        ActionBar ac = getSupportActionBar();
+        ac.setTitle("도형 인식 부분");
+
+        DrawView drawView = findViewById(R.id.drawView);
+        drawView.setStrokeWidth(60.0f);
+        drawView.setBackgroundColor(Color.WHITE);
+        drawView.setColor(Color.BLACK);
+
+        TextView resultView = findViewById(R.id.resultView);
+
+        Button classifyBtn = findViewById(R.id.classifyBtn);
+        classifyBtn.setOnClickListener(v -> {
+            Bitmap image = drawView.getBitmap();
+
+            Pair<Integer, Float> res = cls.classify(image);
+            String outStr = String.format(Locale.ENGLISH, "%d, %.0f%%", res.first, res.second * 100.0f);
+            resultView.setText(outStr);
+        });
+
+        Button clearBtn = findViewById(R.id.clearBtn);
+        clearBtn.setOnClickListener(v -> {
+            drawView.clearCanvas();
+        });
+
+        cls = new Classifier(this);
+        try {
+            cls.init();
+        } catch(IOException ioe) {
+            Log.d("DigitClassifier", "failed to init Classifier", ioe);
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        cls.finish();
+        super.onDestroy();
+    }
+
+}
--- LEDCUBE/app/src/main/java/com/example/ledcube/gmselect/SelectActivity.java
+++ LEDCUBE/app/src/main/java/com/example/ledcube/gmselect/SelectActivity.java
@@ -2,9 +2,12 @@ package com.example.ledcube.gmselect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.ledcube.R;
+import com.example.ledcube.draw.DrawActivity;
 
 public class SelectActivity extends AppCompatActivity {
 
@@ -14,6 +17,15 @@ public class SelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameselect);
 
+        Button game1 = findViewById(R.id.button1);
+
+
+
+        Button game2 = findViewById(R.id.button2);
+        game2.setOnClickListener(view -> {
+            Intent i = new Intent(SelectActivity.this, DrawActivity.class);
+            startActivity(i);
+        });
 
     }
 }
