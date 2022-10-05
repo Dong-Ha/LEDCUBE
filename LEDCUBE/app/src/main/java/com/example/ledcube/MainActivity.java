043357680d64d57d0a66664dd81d3f6a377bb064
Shindongha <tlsehdgk33@gmail.com>
2022-10-05 10:13:00 +0900

    1

---
+++ LEDCUBE/app/src/main/java/com/example/ledcube/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ledcube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.ledcube.ble.BleActivity;
+import com.example.ledcube.gmselect.SelectActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button gamestart = findViewById(R.id.btnstart);
+        gamestart.setOnClickListener(view -> {
+            Intent i = new Intent( MainActivity.this, SelectActivity.class);
+            startActivity(i);
+        });
+
+        Button blesetting = findViewById(R.id.btnsetting);
+        blesetting.setOnClickListener(view -> {
+            Intent i = new Intent( MainActivity.this, BleActivity.class);
+            startActivity(i);
+        });
+
+
+
+    }
+}
---
+++ LEDCUBE/app/src/main/java/com/example/ledcube/gmselect/SelectActivity.java
@@ -0,0 +1,19 @@
+package com.example.ledcube.gmselect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ledcube.R;
+
+public class SelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gameselect);
+
+
+    }
+}
